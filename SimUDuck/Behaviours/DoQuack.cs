325bb54eb80586a9871a038c3f28eb7e1159d491
Surya <bhnsurya200@gmail.com>
2021-07-08 16:24:26 -0500

    Implementing SimUDuck app

---
+++ SimUDuck/Behaviours/DoQuack.cs
@@ -0,0 +1,15 @@
+﻿using SimUDuck.Interface;
+using System;
+
+
+namespace SimUDuck.Behaviours
+{
+    public class DoQuack : IQuackBehavior
+    {
+        public void Quack()
+        {
+            Console.WriteLine("Quack .... Quack ....");
+        }
+    }
+    
+}
---
+++ SimUDuck/Interface/IQuackBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimUDuck.Interface
+{
+
+    //Interface for QuackBehavior
+    public interface IQuackBehavior
+    {
+        public void Quack();
+    }
+}
